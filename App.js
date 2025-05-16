cc4d16d6a2ceb5a9063a6bcadab0c035ceb9e882
SmpManeth <smpmaneth@gmail.com>
2025-05-16 15:53:20 +0530

    build(deps): update App.js, TabBottomMenu.style.js, package-lock.json and package.json

--- App.js
+++ App.js
@@ -6,23 +6,13 @@ import { CardTodo } from "./components/CardTodo/CardTodo";
 import { useState } from "react";
 import { TabBottomMenu } from "./components/TabBottomMenu/TabBottomMenu";
 import { ButtonAdd } from "./components/ButtonAdd/ButtonAdd";
+import Dialog from "react-native-dialog";
 
 export default function App() {
-
   const [slelectedTabName, setSelectedTabName] = useState("all");
+  const [showAddDialog, setShowAddDialog] = useState(false);
+  const [todoList, setTodoList] = useState([]);
 
-  const [todoList, setTodoList] = useState([
-    { id: 1, title: "Todo 1", isCompleted: true },
-    { id: 2, title: "Todo 2", isCompleted: false },
-    { id: 3, title: "Todo 3", isCompleted: false },
-    { id: 4, title: "Todo 4", isCompleted: true },
-    { id: 5, title: "Todo 5", isCompleted: false },
-    { id: 6, title: "Todo 6", isCompleted: true },
-    { id: 7, title: "Todo 7", isCompleted: false },
-    { id: 8, title: "Todo 8", isCompleted: true },
-  ]);
-
-  
   function getFileteredTodoList() {
     if (slelectedTabName === "all") {
       return todoList;
@@ -34,19 +24,27 @@ export default function App() {
   }
 
   function renderTodoList() {
-    return getFileteredTodoList().map((todo) => <CardTodo onLongPress={deleteTodo} onPress={updateTodo} key={todo.id} todo={todo} />);
+    return getFileteredTodoList().map((todo) => (
+      <CardTodo
+        onLongPress={deleteTodo}
+        onPress={updateTodo}
+        key={todo.id}
+        todo={todo}
+      />
+    ));
   }
 
-  function updateTodo(todo){
+  function updateTodo(todo) {
     const updatedTodo = {
-      ...todo , isCompleted: !todo.isCompleted
+      ...todo,
+      isCompleted: !todo.isCompleted,
     };
     setTodoList(
       todoList.map((item) => (item.id === todo.id ? updatedTodo : item))
     );
   }
 
-  function deleteTodo(todo){
+  function deleteTodo(todo) {
     Alert.alert(
       "Delete Todo",
       `Are you sure you want to delete ${todo.title}?`,
@@ -67,20 +65,48 @@ export default function App() {
     );
   }
 
+  function renderAddDialog() {
+    return (
+      <Dialog.Container
+        visible={showAddDialog}
+        onBackdropPress={() => setShowAddDialog(false)}
+      >
+        <Dialog.Title>Add Todo</Dialog.Title>
+        <Dialog.Description>Choose a Name to Your Todo</Dialog.Description>
+        <Dialog.Input placeholder="Todo Title" />
+        <Dialog.Button label="Cancel" color="grey" onPress={() => {setShowAddDialog(false)}} />
+        <Dialog.Button label="Save" onPress={() => {}} />
+      </Dialog.Container>
+    );
+  }
+
+  function showAddTodoDialog() {
+    setShowAddDialog(true);
+  }
+
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.app}>
-        <View style={styles.header}>
-          <Header />
-        </View>
-        <View style={styles.body}>
-          <ScrollView>{renderTodoList()}</ScrollView>
-        </View>
-        <ButtonAdd />
-        <View style={styles.footer}>
-         <TabBottomMenu todoList={todoList} onPress={setSelectedTabName} slelectedTabName={slelectedTabName} />
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.app}>
+          <View style={styles.header}>
+            <Header />
+          </View>
+          <View style={styles.body}>
+            <ScrollView>{renderTodoList()}</ScrollView>
+          </View>
+
+          <ButtonAdd onPress={showAddTodoDialog} />
+
+          <View style={styles.footer}>
+            <TabBottomMenu
+              todoList={todoList}
+              onPress={setSelectedTabName}
+              slelectedTabName={slelectedTabName}
+            />
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      {renderAddDialog()}
+    </>
   );
 }
